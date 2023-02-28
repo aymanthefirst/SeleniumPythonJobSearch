457819af715bce6612969ee3199e13bbab82122b
Ayman Harake <42536841+aymanthefirst@users.noreply.github.com>
2023-02-28 08:58:02 +0000

    Delete govChat.py

--- govChat.py
+++
@@ -1,7 +0,0 @@
-from selenium.webdriver.common.by import By
-from selenium import webdriver
-
-driver = webdriver.Chrome()
-
-driver.get("https://www.tax.service.gov.uk/ask-hmrc/webchat/income-tax-enquiries-for-individuals-pensioners-and-employees")
-
